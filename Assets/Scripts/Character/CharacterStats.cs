be7cab88633f1ab363690f65b6d65cefa291a60b
Dr. 27 <dr27draws@gmail.com>
2024-06-01 18:00:12 -0500

    Font & Coins Setup

---
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -0,0 +1,14 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int Coins;
+    [SerializeField] private TextMeshProUGUI _coinsText;
+    
+    private void Update()
+    {
+        _coinsText.text = Coins.ToString();
+    }
+}
